189f1edd43244524790e395eaef93e18402cc56b
Bryan Alexis <66497776+Bryan0208@users.noreply.github.com>
2020-09-05 23:26:23 -0400

    Update estanque.ino

--- estanque.ino
+++ estanque.ino
@@ -1,12 +1,13 @@
-#include <Servo.h>
-int pos = 0;
+#include <Servo.h> //librería para servo motor
+int pos = 0; 
 int servo1=9;
 int servo2=10;
 int servo3=11;
-Servo valv1;
-Servo valv2;
-Servo valv3;
+Servo valv1;  //se define a la valvula 1 como variable servo
+Servo valv2;  //se define a la valvula 2 como variable servo
+Servo valv3;  //se define a la valvula 3 como variable servo
 float adc0, temp, mV; //se declaran variables para sensor de temperatura
+
 void setup() {
     pinMode(2,OUTPUT);    //configura pin 2 como salida
     pinMode(3,OUTPUT);    //configura pin 3 como salida digital
@@ -20,11 +21,11 @@ void loop() {
   mV= (adc0*5)/1024;      //se convierte valor digital a mV
   temp = mV * 100;        //convertimos los mV a temp
 
-  if(temp < 80)
+  if(temp < 80)   //si la temperatura es menor a 80 se cierra la valvula 3
   {
     valv3.write(0);
   }
-    if(temp >=80)
+    if(temp >=80)  //si la temperatura es mayor o igual a 80 se abre la valvula 3
   {
     valv3.write(180);
   }
