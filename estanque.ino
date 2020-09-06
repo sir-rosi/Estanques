abedcfe7d1ab508b57d619e90f60c0b90eab96d1
sir-rosi <66852573+sir-rosi@users.noreply.github.com>
2020-09-05 23:38:45 -0400

    Update estanque.ino
    agregue + comentarios

--- estanque.ino
+++ estanque.ino
@@ -1,8 +1,8 @@
 #include <Servo.h> //librería para servo motor
 int pos = 0; 
-int servo1=9;
-int servo2=10;
-int servo3=11;
+int servo1=9;//-----|
+int servo2=10;//----|se seleccionan pines con pwm.
+int servo3=11;//----|
 Servo valv1;  //se define a la valvula 1 como variable servo
 Servo valv2;  //se define a la valvula 2 como variable servo
 Servo valv3;  //se define a la valvula 3 como variable servo
