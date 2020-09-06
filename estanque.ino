5d20f33e40104812258c3f2bac07e03cf52744c8
Bryan Alexis <66497776+Bryan0208@users.noreply.github.com>
2020-09-05 23:45:39 -0400

    Update estanque.ino

--- estanque.ino
+++ estanque.ino
@@ -1,11 +1,26 @@
 #include <Servo.h> //librería para servo motor
 int pos = 0; 
-int servo1=9;//-----|
-int servo2=10;//----|se seleccionan pines con pwm.
-int servo3=11;//----|
+int servo1=9;
+int servo2=10;
+int servo3=11;
 Servo valv1;  //se define a la valvula 1 como variable servo
 Servo valv2;  //se define a la valvula 2 como variable servo
 Servo valv3;  //se define a la valvula 3 como variable servo
+
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Inicializar LOW para limpiar trigger por 2 microsegundos
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Inicializar trigger en HIGH por 8 microsegundos para comenzar
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(8);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // lectura de pin echo con el retorno de la señal
+  return pulseIn(echoPin, HIGH);
+}
+
 float adc0, temp, mV; //se declaran variables para sensor de temperatura
 
 void setup() {
@@ -17,6 +32,15 @@ void setup() {
 }
 
 void loop() {
+  float distancia = 0.01723 * readUltrasonicDistance(2, 2);
+  if (distancia=100)  
+  {
+    valv2(1);
+  }
+  else (
+  {
+    valv2(0);
+  }
   adc0 = analogRead(0);   //se lee señal analogica 0-1023
   mV= (adc0*5)/1024;      //se convierte valor digital a mV
   temp = mV * 100;        //convertimos los mV a temp
